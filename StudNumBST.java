4fe58e2fafe675ab7cd839981f05f449c98bf03e
mvigneault <mvigneault@deerfield.edu>
2016-12-08 19:31:41 -0500

    Update StudNumBST.java

--- StudNumBST.java
+++ StudNumBST.java
@@ -1,5 +1,4 @@
-/*Program Four- Patrick Davis- 10/31/16
-Binary Search Tree*/
+/* Binary Search Tree by Student Number*/
 
 public class StudNumBST{
 	//declare the root/ top of the tree
@@ -20,70 +19,70 @@ public class StudNumBST{
 		//if the node we want to delete is the root/ top of the tree
 		if (p.getStudentNumber()== top.getStudentNumber()){
 			StudentNode successor = getSuccessor(p);
-			successor.setLeft(p.getLeft());
-			successor.setRight(p.getRight());
-			p.setLeft(null);
-			p.setRight(null);
+			successor.setStudNumLeft(p.getStudNumLeft());
+			successor.setStudNumRight(p.getStudNumRight());
+			p.setStudNumLeft(null);
+			p.setStudNumRight(null);
 			top=successor;
 		}
 		//if the Node we want to delete is a leaf with no children
-		else if (p.getLeft()== null && p.getRight()==null){
+		else if (p.getStudNumLeft()== null && p.getStudNumRight()==null){
 			//get the parent of the node we want to delete
 			//must have this in every else statement because cant get the parent of top
 			StudentNode parent = getParent(p);
 			//if node to delete is to the right of the parent
 			if (parent.getStudentNumber()<p.getStudentNumber()){
-				parent.setRight(null);
+				parent.setStudNumRight(null);
 			}
 			//else if the node to delete is to the left of the parent
 			else{
-				parent.setLeft(null);
+				parent.setStudNumLeft(null);
 			}
 		}
 		//if the node to delete only has nodes coming off its left side
-		else if(p.getRight()==null){
+		else if(p.getStudNumRight()==null){
 			StudentNode parent = getParent(p);
 			//if node to delete is to the right of the parent
 			if (parent.getStudentNumber()<p.getStudentNumber()){
-				parent.setRight(p.getLeft());
-				p.setLeft(null);
+				parent.setStudNumRight(p.getStudNumLeft());
+				p.setStudNumLeft(null);
 			}
 			//else if the node to delete is to the left of the parent
 			else{
-				parent.setLeft(p.getLeft());
-				p.setLeft(null);
+				parent.setStudNumLeft(p.getStudNumLeft());
+				p.setStudNumLeft(null);
 			}
 			
 		}
 		//if the node to delete only has nodes coming off its right side
-		else if(p.getLeft()==null){
+		else if(p.getStudNumLeft()==null){
 			StudentNode parent = getParent(p);
-			if (parent.getKey()<p.getKey()){
-				parent.setRight(p.getRight());
-				p.setRight(null);
+			if (parent.getStudentNumber()<p.getStudentNumber()){
+				parent.setStudNumRight(p.getStudNumRight());
+				p.setStudNumRight(null);
 			}
 			else{
-				parent.setLeft(p.getRight());
-				p.setRight(null);
+				parent.setStudNumLeft(p.getStudNumRight());
+				p.setStudNumRight(null);
 			}
 		}
 		//if the node to delete has two children coming off of it
-		else if (p.getLeft()!= null && p.getRight()!=null){
+		else if (p.getStudNumLeft()!= null && p.getStudNumRight()!=null){
 			//find the parent and the successor
 			StudentNode parent = getParent(p);
 			StudentNode successor = getSuccessor(p);
 			//set the successors left and right to the left and right Node your deleting
-			successor.setLeft(p.getLeft());
-			successor.setRight(p.getRight());
+			successor.setStudNumLeft(p.getStudNumLeft());
+			successor.setStudNumRight(p.getStudNumRight());
 			//remove the node completely from the tree
-			p.setLeft(null);
-			p.setRight(null);
+			p.setStudNumLeft(null);
+			p.setStudNumRight(null);
 			//know which side of the parent to set to the successor
 			if (parent.getStudentNumber()<p.getStudentNumber()){
-				parent.setRight(successor);
+				parent.setStudNumRight(successor);
 			}
 			else{
-				parent.setLeft(successor);
+				parent.setStudNumLeft(successor);
 			}
 		}
 	}
@@ -95,40 +94,40 @@ public class StudNumBST{
 	//recursive parent
 	public StudentNode reGetParent(StudentNode temp, int studentNumber){
 		//if the node your looking for if less than the parent check to the left
-		if (key < temp.getStudentNumber()){
-			if (temp.getLeft().getStudentNumber()==studentNumber){
+		if (studentNumber < temp.getStudentNumber()){
+			if (temp.getStudNumLeft().getStudentNumber()==studentNumber){
 				return temp;
 			}
 			else{
-				return reGetParent(temp.getLeft(), studentNumber);
+				return reGetParent(temp.getStudNumLeft(), studentNumber);
 			}
 		}
 		//if the node your looking for is greater than the parent check to the right
 		else {
-			if (temp.getRight().getStudentNumber()==studentNumber){
+			if (temp.getStudNumRight().getStudentNumber()==studentNumber){
 				return temp;
 			}
 			else{
-				return reGetParent(temp.getRight(), studentNumber);
+				return reGetParent(temp.getStudNumRight(), studentNumber);
 			}
 		}
 	}
 	//get successor
 	public StudentNode getSuccessor(StudentNode p){
 		//get to the left most node off of the right of p
-		StudentNode temp = p.getRight();
-		while (temp.getLeft() != null){
-			temp = temp.getLeft();
+		StudentNode temp = p.getStudNumRight();
+		while (temp.getStudNumLeft() != null){
+			temp = temp.getStudNumLeft();
 		}
 		//get parent and check if its bigger or less to remove the successor from the tree
 		//could also call delete on it
 		StudentNode parent = getParent(temp);
 		if (parent.getStudentNumber()<temp.getStudentNumber()){
-			parent.setRight(temp.getRight());
+			parent.setStudNumRight(temp.getStudNumRight());
 		}
 		else{
-			parent.setLeft(temp.getRight());
-			temp.setRight(null);
+			parent.setStudNumLeft(temp.getStudNumRight());
+			temp.setStudNumRight(null);
 		}
 		
 		return temp;
@@ -146,19 +145,19 @@ public class StudNumBST{
 	//recurrsive insert
 	public void reInsert(StudentNode temp, StudentNode p){
 		if (p.getStudentNumber()< temp.getStudentNumber()){
-			if (temp.getLeft() ==null){
-				temp.setLeft(p);
+			if (temp.getStudNumLeft() ==null){
+				temp.setStudNumLeft(p);
 			}
 			else{
-				reInsert(temp.getLeft(),p);
+				reInsert(temp.getStudNumLeft(),p);
 			}
 		}
 		else{
-			if(temp.getRight()==null){
-				temp.setRight(p);
+			if(temp.getStudNumRight()==null){
+				temp.setStudNumRight(p);
 			}
 			else{
-				reInsert(temp.getRight(),p);
+				reInsert(temp.getStudNumRight(),p);
 			}
 		}
 	}
@@ -178,10 +177,10 @@ public class StudNumBST{
 		}
 		//if the value is less than or bigger, move left or right in the tree
 		else if (studentNumber < temp.getStudentNumber()){
-			return compareNode(temp.getLeft(),studentNumber);
+			return compareNode(temp.getStudNumLeft(),studentNumber);
 		}
 		else {
-			return compareNode(temp.getRight(),studentNumber);
+			return compareNode(temp.getStudNumRight(),studentNumber);
 		}
 	}
 	
@@ -191,11 +190,11 @@ public class StudNumBST{
 		System.out.println();
 	}
 	public void reTraverse(StudentNode temp){
-		// aslong as its not null keep traversing
+		// as long as its not null keep traversing
 		if (temp != null){
-			reTraverse(temp.getLeft());
+			reTraverse(temp.getStudNumLeft());
 			System.out.print(temp.getStudentNumber()+" ");
-			reTraverse(temp.getRight());
+			reTraverse(temp.getStudNumRight());
 		}
 		
 	}
@@ -210,18 +209,16 @@ public class StudNumBST{
 	private void printTree2(StudentNode tree) {
 		if (tree != null) {
 			System.out.print(tree.getStudentNumber() + " ");
-			if (tree.getLeft() != null)
-				System.out.print("Left: " + tree.getLeft().getStudentNumber() + " ");
+			if (tree.getStudNumLeft() != null)
+				System.out.print("Left: " + tree.getStudNumLeft().getStudentNumber() + " ");
 			else
 				System.out.print("Left: null ");
-			if (tree.getRight() != null)
-				System.out.println("Right: " + tree.getRight().getStudentNumber() + " ");
+			if (tree.getStudNumRight() != null)
+				System.out.println("Right: " + tree.getStudNumRight().getStudentNumber() + " ");
 			else
 				System.out.println("Right: null ");
-			printTree2(tree.getLeft());
-			printTree2(tree.getRight());
+			printTree2(tree.getStudNumLeft());
+			printTree2(tree.getStudNumRight());
 		}
 	}
-
-	
 }
