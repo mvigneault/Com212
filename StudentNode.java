140eef316992fc6364a5c2cb7efb686c37ea8c03
mvigneault <mvigneault@deerfield.edu>
2016-12-08 19:13:49 -0500

    Update StudentNode.java

--- StudentNode.java
+++ StudentNode.java
@@ -1,18 +1,19 @@
 //Student Node class - Mathieu Vigneault & Patrick Davis & Kendra Bolt
 public class StudentNode {
-	
-	//Declare variables 
+
 	private int SSN;
 	private String email;
 	private String name;
 	private int studentNumber;
 	private IdeaNode originalIdea;
 	private IdeaQueueLL queue;
-	private StudentNode Left;
-	private StudentNode Right;
-	private averageRating;
+	private StudentNode SSNLeft; 
+	private StudentNode SSNRight;
+	private StudentNode StudNumLeft; 
+	private StudentNode StudNumRight;
 	
-	//contructor that creates a node with a student name, SSN, email and studentNumber.
+	
+	//contructor that creates a node with a name, SSN
 	public StudentNode (String name, int SSN, String email, int studentNumber){
 		this.name = name; 
 		this.SSN = SSN;
@@ -27,7 +28,7 @@ public class StudentNode {
 		this.SSN = SSN;
 		this.email = email;
 		this.studentNumber = studentNumber;
-		this.originalIdea = originalIdea;
+		this.setOriginalIdea(originalIdea);
 		
 	}
 
@@ -63,24 +64,9 @@ public class StudentNode {
 		this.studentNumber = studentNumber;
 	}
 	
-	public StudentNode getRight() {
-		return Right;
-	}
-
-	public void setRight(StudentNode Right) {
-		this.Right = Right;
-	}
-
-	public StudentNode getLeft() {
-		return Left;
-	}
-
-	public void setLeft(StudentNode Left) {
-		this.Left = Left;
-	}
-	
 	public void addIdea(IdeaNode idea) {
-		queue.enqueue(idea);
+				queue.enqueue(idea);
+			
 	}
 	
 	public double getAverageRating(){
@@ -98,6 +84,46 @@ public class StudentNode {
 	
 	public void printIdeaQueue(){
 		queue.printQueue();
+	}
+
+	public IdeaNode getOriginalIdea() {
+		return originalIdea;
+	}
+
+	public void setOriginalIdea(IdeaNode originalIdea) {
+		this.originalIdea = originalIdea;
+	}
+
+	public StudentNode getSSNLeft() {
+		return SSNLeft;
+	}
+
+	public void setSSNLeft(StudentNode sSNLeft) {
+		SSNLeft = sSNLeft;
+	}
+
+	public StudentNode getSSNRight() {
+		return SSNRight;
+	}
+
+	public void setSSNRight(StudentNode sSNRight) {
+		SSNRight = sSNRight;
+	}
+
+	public StudentNode getStudNumLeft() {
+		return StudNumLeft;
+	}
+
+	public void setStudNumLeft(StudentNode studNumLeft) {
+		StudNumLeft = studNumLeft;
+	}
+
+	public StudentNode getStudNumRight() {
+		return StudNumRight;
+	}
+
+	public void setStudNumRight(StudentNode studNumRight) {
+		StudNumRight = studNumRight;
 	}
 	
 }
