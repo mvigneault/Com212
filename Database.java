06ecf377fe64f5fd38b2c2eb26b4252df54d5e22
mvigneault <mvigneault@deerfield.edu>
2016-12-19 11:58:20 -0500

    Update Database.java

--- Database.java
+++ Database.java
@@ -1,91 +1,83 @@
-public class Database{
-  public StudentNode studentRecord;
-  private int uniqueIdeaNumber = 0;
-  public SSNTree ssnTree;
-  public StudNumBST studNumBST;
-  public IdeaHeap ideaHeap;
+/*This is the Database Class. 
+ * It is a class that helps regroup all the methods to make it easy to call them. 
+ * This class was done by Kendra.
+ * It was revised by Patrick and Mathieu.
+*/
+
+import java.io.*;
+
+public class Database implements java.io.Serializable{
+	public StudentNode studentRecord;
+	private int uniqueIdeaNumber = 0;
+	public SSNTree ssnTree;
+	public StudNumBST studNumBST;
+	public IdeaHeap ideaHeap;
   
-  public Database(){
-    ssnTree = new SSNTree();
-    studNumBST = new StudNumBST();
-    ideaHeap = new IdeaHeap();
-  }
+	public Database() {
+		ssnTree = new SSNTree();
+		studNumBST = new StudNumBST();
+		ideaHeap = new IdeaHeap();
+	}
   
-  public StudentNode searchSSN(int ssn){
-    //searches SSNtree to return StudentNode
-    //studentRecord = student node from search (for manipulation)
-    studentRecord = ssnTree.search(ssn); //******************
+	public StudentNode searchSSN(int ssn){
+		studentRecord = ssnTree.search(ssn); 
+		return studentRecord;
+	}
 
-    //displays last name, email login, ssn, avg score ideas, ideaqueue
-   // System.out.println(studentRecord.getName());
-    //System.out.println(studentRecord.getEmail());
-    //System.out.println(studentRecord.getSSN());
-    //System.out.println(studentRecord.getAverageRating());
-    //System.out.println();
-    //studentRecord.printIdeaQueue();
-	return studentRecord;
-  }
+	public void changeLastName(String newLastName){
+		studentRecord.setName(newLastName);
+	}
 
-  public void changeLastName(String newLastName){
-    studentRecord.setName(newLastName);
-  }
+	public void changeEmail(String newEmail){
+		studentRecord.setEmail(newEmail); 
+	}
 
-  public void changeEmail(String newEmail){
-    studentRecord.setEmail(newEmail); 
-  }
-
-  public void deleteRecord(){
+	public void deleteRecord(){
     //Remove studentRecord from SSNtree and StudentIDtree
-    ssnTree.delete(studentRecord);
-    studNumBST.delete(studentRecord);
-  }
+		ssnTree.delete(studentRecord);
+		studNumBST.delete(studentRecord);
+	}
 
-  public void newStudent(StudentNode student){
-    //add student to SSNtree and StudentID tree
-    ssnTree.insert(student);
-    studNumBST.insert(student);
-  }
+	public void newStudent(StudentNode student){
+	//add student to SSNtree and StudentID tree
+		ssnTree.insert(student);
+		studNumBST.insert(student);
+	}
 
-  public void newIdea(IdeaNode idea){
-    idea.setIdeaNum(uniqueIdeaNumber);
-    //add new idea to IdeaHeap and use ideaSSN to add idea to the correct student
-    int ssn = idea.getSSN();
-    ideaHeap.insert(idea);
-    uniqueIdeaNumber++;
-    studentRecord = ssnTree.search(ssn);
-    studentRecord.addIdea(idea);
-  }
+	public void newIdea(IdeaNode idea){
+		idea.setIdeaNum(uniqueIdeaNumber);
+		//add new idea to IdeaHeap and use ideaSSN to add idea to the correct student
+		int ssn = idea.getSSN();
+		ideaHeap.insert(idea);
+		uniqueIdeaNumber++;
+		studentRecord = ssnTree.search(ssn);
+		studentRecord.addIdea(idea);
+	}
 
-  public IdeaNode getBestIdea(){
-    return ideaHeap.findMax();
-  }
+	public IdeaNode getBestIdea(){
+		return ideaHeap.findMax();
+	}
 
-  public IdeaNode sellBestIdea(){
-	  IdeaNode bestIdea = ideaHeap.findMax();
-	  ideaHeap.deleteMax();
-	  return bestIdea;
-  }
+	public IdeaNode sellBestIdea(){
+		IdeaNode bestIdea = ideaHeap.findMax();
+		ideaHeap.deleteMax();
+		return bestIdea;
+	}
 
-  public String getEmail (int num){  //num is student ID number. 
-    //search student number tree for student
-    studentRecord = studNumBST.search(num);
-    if(studentRecord == null){
-    	return null;
-    }
-    else{//display email login
-    return studentRecord.getEmail();
-    }
-  }
+	public String getEmail (int num){  //num is student ID number. 
+	//search student number tree for student
+		studentRecord = studNumBST.search(num);
+		if(studentRecord == null){
+			return null;
+	    }
+		else{//display email login
+			return studentRecord.getEmail();
+		}
+	}
 
-  public void printStudents(){
-    //print student ID tree
-    //students printed must also display student number, name, SSN, average score
-    studNumBST.printTree();
-  }
-
-  //Save and Load functions
-
-  //Your program should store the contents of the database to a file when it is shut down and retrieve the data when started back up again.
-  //You only need a single text file for this, but can use more if you prefer.
-	
+	public void printStudents(){
+		//print student ID tree
+		//students printed must also display student number, name, SSN, average score
+		studNumBST.printTree();
+	}
 }
